3793743cd38c729961436f88fadaded106ba918b
itachigorun <itachigorun@gmail.com>
2018-12-27 14:14:12 +0800

    main

--- designPattern/singleton/src/Singleton2.java
+++ designPattern/singleton/src/Singleton2.java
@@ -10,10 +10,16 @@
  */
 public class Singleton2 {
     private static Singleton2 instance = new Singleton2();
-    private Singleton2(){}
+
+    private Singleton2() {
+    }
 
     public static Singleton2 getInstance() {
         return instance;
     }
 
+
+    public static void main(String[] args) {
+        Singleton2 instance = Singleton2.getInstance();
+    }
 }
