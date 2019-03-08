596b21d7e2d3945e4e608e1117cac6c1b473c4d2
itachigorun <itachigorun@gmail.com>
2019-03-08 16:39:27 +0800

    统一文件夹名

--- designPattern/Bridge/Qiao.java
+++
@@ -1,9 +0,0 @@
-/**
- * Description:
- *
- * @author houkepan
- * @date 2019/3/8 13:41
- */
-public interface Qiao {
-    void targetAreaB();
-}
--- designPattern/Proxy/IBuyCar.java
+++
@@ -1,9 +0,0 @@
-/**
- * Description:
- *
- * @author houkepan
- * @date 2019/3/8 11:08
- */
-public interface IBuyCar {
-    void buyCar();
-}
