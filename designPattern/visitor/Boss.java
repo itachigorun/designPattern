7896a3be676f33061c01f79fbaa995fcdf7e007d
itachigorun <itachigorun@gmail.com>
2019-03-09 23:47:28 +0800

    访问者模式

--- designPattern/visitor/Boss.java
+++ designPattern/visitor/Boss.java
@@ -14,6 +14,7 @@ public class Boss implements AccountBookViewer {
         totalConsume += bill.getAmount();
     }
 
+    @Override
     public void view(IncomeBill bill) {
         totalIncome += bill.getAmount();
     }
